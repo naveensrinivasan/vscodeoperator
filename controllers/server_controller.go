61c560f84ae1f91e337b9c7e6808e0a88e946e41
naveen <172697+naveensrinivasan@users.noreply.github.com>
2020-10-29 14:11:15 -0400

    Fixed the namespace issue, that was causing deployments to be skipped. Implemented filtering of the events.

--- controllers/server_controller.go
+++ controllers/server_controller.go
@@ -18,14 +18,18 @@ package controllers
 
 import (
 	"context"
-
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	vscodev1alpha1 "github.com/naveensrinivasan/api/v1alpha1"
 )
@@ -42,16 +46,28 @@ type ServerReconciler struct {
 
 // +kubebuilder:rbac:groups=apps,resources=deployments,verbs=list;watch;get;patch
 // +kubebuilder:rbac:groups=core,resources=services,verbs=list;watch;get;patch
+// +kubebuilder:rbac:groups=apps,resources=namespace,verbs=list;watch;get
 func (r *ServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	const CodeServer = "codeserver"
+	var code vscodev1alpha1.Server
+
 	ctx := context.Background()
 	log := r.Log.WithValues("codeserver", req.NamespacedName)
 
-	log.Info("reconciling codeserver")
-
-	var code vscodev1alpha1.Server
 	if err := r.Get(ctx, req.NamespacedName, &code); err != nil {
+		if apierrors.IsNotFound(err) {
+			if req.Namespace == "" {
+				r.Create(ctx, &vscodev1alpha1.Server{
+					ObjectMeta: metav1.ObjectMeta{Namespace: req.Name, Name: CodeServer},
+				})
+				log.Info("scheduled for namespace", "namespace", req.Name)
+				return ctrl.Result{}, nil
+			}
+			return ctrl.Result{}, nil
+		}
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
+
 	deployment, err := r.desiredDeployment(code)
 	if err != nil {
 		return ctrl.Result{}, err
@@ -82,5 +98,23 @@ func (r *ServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&vscodev1alpha1.Server{}).
 		Owns(&corev1.Service{}).
 		Owns(&appsv1.Deployment{}).
-		Complete(r)
+		Watches(&source.Kind{Type: &corev1.Namespace{}},
+			&handler.EnqueueRequestForObject{}).
+		WithEventFilter(predicate.Funcs{CreateFunc: func(event event.CreateEvent) bool {
+			// Look for only namespace and vscode server
+			_, ok := event.Object.(*vscodev1alpha1.Server)
+			if ok {
+				return true
+			}
+			// Only for namespaces that have specific labels
+			ns, e := event.Object.(*corev1.Namespace)
+			if e {
+				for k, _ := range ns.Labels {
+					if k == "code-server" {
+						return true
+					}
+				}
+			}
+			return false
+		}}).Complete(r)
 }
